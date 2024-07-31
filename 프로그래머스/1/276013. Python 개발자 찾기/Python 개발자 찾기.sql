1ebb46630d49e4b7b69d3c3f4f6288b9a3f55f61
coldrain-f <intmain__@naver.com>
2024-07-31 22:21:05 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -4,7 +4,5 @@ SELECT ID
     , LAST_NAME
 FROM DEVELOPER_INFOS
 WHERE 1=1
-AND SKILL_1 = 'Python' 
-    OR SKILL_2 = 'Python'
-    OR SKILL_3 = 'Python'
+AND 'Python' IN (SKILL_1, SKILL_2, SKILL_3)
 ORDER BY ID
